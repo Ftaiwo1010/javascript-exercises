21d22979285e7eb6d837dc92dd9ec61f44cb368a
Femi <ftaiwo1010@gmail.com.noreply.github.com>
2025-07-11 10:42:24 +0100

    finished 11_tempCoversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(c) {
+    let toCelsius = (c - 32) * 5/9;
+    return Number(toCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+    let toFahrenheit = (f * 9/5) + 32;
+    return Number(toFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
