565d667af32f83d00631f5048fc260256e0b2f73
Femi <ftaiwo1010@gmail.com.noreply.github.com>
2025-07-24 18:43:50 +0100

    finished 16_findTheOldest exercise. used reduce to achieve the desired result

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,43 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    //   const currentYear = new Date().getFullYear();
+
+    //   const getAge = (birth, death) => {
+    //      if (!death) {
+    //          death = currentYear;
+    //      }   
+
+    //      return death - birth;
+    //   };
+
+    //   return people.reduce((oldest, person) => {
+    //      const oldestAge = getAge(
+    //         oldest.yearOfBirth,
+    //         oldest.yearOfDeath
+    //      );
+
+    //      const currentPersonAge = getAge(
+    //         person.yearOfBirth,
+    //         person.yearOfDeath
+    //      );
+
+    //      return oldestAge > currentPersonAge ? oldest : person;
+
+    //   });
+
+    // shorter version
+     const currentYear = new Date().getFullYear();
+
+     return people.reduce((oldest, person) => {
+    
+     const oldestYearDeath = oldest.yearOfDeath ?? currentYear;
+     const oldestAge = oldestYearDeath - oldest.yearOfBirth;
+
+     const personYearDeath = person.yearOfDeath ?? currentYear;
+     const currentPersonAge = personYearDeath - person.yearOfBirth;
+
+     return oldestAge > currentPersonAge ? oldest : person;
+
+   });
 
 };
 
