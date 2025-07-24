d671bbfebf218b3612f9f348a6cefd7a005c87c6
Femi <ftaiwo1010@gmail.com.noreply.github.com>
2025-07-24 18:52:39 +0100

    finished 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   // Normalize the string: lowercase, remove all non-alphanumeric characters
+  const cleaned = str
+    .toLowerCase()
+    .replace(/[^a-z0-9]/g, ''); // only letters and digits
 
+  // Compare the cleaned string with its reverse
+  const reversed = cleaned.split('').reverse().join('');
+  
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
